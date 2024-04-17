e1dd1613af0699d8da535152f6a107e434e1edb8
Andre Borges Medeiros <andborges@gmail.com>
2024-04-17 20:59:21 -0300

    Sending api-key on login

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -1,40 +1,42 @@
 import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
-import { random, authentication, encrypt } from '../helpers';
+import { random, authentication, encrypt, decrypt } from '../helpers';
 import { createClient, getClientByEmail, getClientBySessionToken } from '../db/client';
 import config from '../config/index';
 
 export const login = async (req: express.Request, res: express.Response) => {
   try {
-    const { email, password } = req.body
+    const { email, password } = req.body;
 
     if (!email || !password) {
-      return res.sendStatus(400)
+      return res.sendStatus(400);
     }
 
-    const client = await getClientByEmail(email).select('+authentication.salt +authentication.password +authentication.apiKey')
+    const client = await getClientByEmail(email).select('+authentication.salt +authentication.password +authentication.apiKey');
 
     if (!client || !client.authentication || !client.authentication.salt || !client.authentication.password) {
-      return res.sendStatus(404)
+      return res.sendStatus(404);
     }
 
-    const expectedHash = authentication(password, client.authentication.salt)
+    const expectedHash = authentication(password, client.authentication.salt);
 
     if (client.authentication.password !== expectedHash) {
-      return res.sendStatus(403)
+      return res.sendStatus(403);
     }
 
-    const salt = random()
-    client.authentication.sessionToken = authentication(salt, client._id.toString())
+    const salt = random();
+    client.authentication.sessionToken = authentication(salt, client._id.toString());
 
-    await client.save()
+    await client.save();
 
     res.cookie("SESSION_TOKEN", client.authentication.sessionToken, {
       secure: true,
       httpOnly: true,
       sameSite: 'none',
       maxAge: 1000 * 60 * 60 * 24 * 7
-    })
+    });
+
+    client.authentication.apiKey = decrypt(client.authentication.apiKey, config.SECRET)
 
     return res.status(200).json(client)
   } catch (error) {
